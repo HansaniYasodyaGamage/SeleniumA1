457878dbfd186f34d7a65b006eda0cedf53a20e8
HansaniYasodyaGamage <hansaniyasodya@gmail.com>
2025-05-02 13:21:03 +0530

    added to open google browser

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,11 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class OpenGoogle {
 
-    public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver\\chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+
+
+
 
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+
+
     }
 }
