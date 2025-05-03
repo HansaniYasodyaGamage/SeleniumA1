2254d978a737c0c26ce7b6e9ba66f7047a165286
HansaniYasodyaGamage <hansaniyasodya@gmail.com>
2025-05-03 14:49:44 +0530

    added to open google browser without webdriver

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,3 +1,4 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -5,15 +6,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class OpenGoogle {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-
-
-
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-
-
-
+        
     }
 }
